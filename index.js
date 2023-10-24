858233cbadc2ee4cb3ce9b61113afb38f6e36834
Andrey Kolenteev <andrii.kolentieiev@gmail.com>
2023-10-24 16:26:26 +0200

    task done v1.1 rows counter added

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const csvParser = require('csv-parser');
 
 // const fileWithData = './files/small_file_for_test.csv';
 const fileWithData = './files/free_company_dataset.csv';
+let counter = 0;
 
 
 fs.createReadStream(fileWithData)
     .pipe(csvParser())
     .on('data', (row) => {
-        console.log(row.country);
+        counter++;
+        console.log(counter, row.country);
     })
     .on('end', () => {
         console.log('Data  ffrom the file in JSON format.');
